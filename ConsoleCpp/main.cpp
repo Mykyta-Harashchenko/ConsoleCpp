c1ac0e2678117d9f3064ea2ce19b1f3e1845cf78
Mykyta-Harashchenko <garasenkonikita538@gmail.com>
2025-12-15 19:57:02 +0000

    Добавьте файлы проекта.

---
+++ ConsoleCpp/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+	std::string name;
+	int health;
+	std::string unit_class;
+	std::cout << "Enter player name:";
+	std::cin >> name;
+	std::cout << "\n";
+	std::cout << "Enter player health:";
+	std::cin >> health;
+	std::cout << "\n";
+	std::cout << "Enter player class (wizard, warrior, archer):";
+	std::cin >> unit_class;
+	std::cout << "\n";
+	std::cout << "Name:" << name << "\n" << "Health:" << health << "\n" << "Class:" << unit_class << "\n";
+}
