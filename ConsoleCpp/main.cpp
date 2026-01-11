507f7d2c84d37706362a46a7f160dc2438180cad
Mykyta-Harashchenko <garasenkonikita538@gmail.com>
2026-01-11 12:09:13 +0000

    complete: homework 19

--- ConsoleCpp/main.cpp
+++ ConsoleCpp/main.cpp
@@ -1,18 +1,43 @@
 ﻿#include <iostream>
 #include <string>
+#include <utility>
+#include <unordered_map>
 
 int main() {
-	std::string name;
-	int health;
-	std::string unit_class;
-	std::cout << "Enter player name:";
-	std::cin >> name;
-	std::cout << "\n";
-	std::cout << "Enter player health:";
-	std::cin >> health;
-	std::cout << "\n";
-	std::cout << "Enter player class (wizard, warrior, archer):";
-	std::cin >> unit_class;
-	std::cout << "\n";
-	std::cout << "Name:" << name << "\n" << "Health:" << health << "\n" << "Class:" << unit_class << "\n";
+	int n;
+	int x, y;
+	std::string command;
+	std::pair<int, int> movement;
+	std::unordered_map<std::string, std::pair<int, int>> movements;
+	movements["right"] = std::make_pair(1, 0);
+	movements["left"] = std::make_pair(-1, 0);
+	movements["forward"] = std::make_pair(0, 1);
+	movements["backward"] = std::make_pair(0, -1);
+	int dxTotal = 0, dyTotal = 0;
+
+	std::cout << "Enter number of iterations:"<< "\n";
+	std::cin >> n;
+	int i = 0;
+	
+	while (i < n) {
+		std::cout << "Enter command (right, left, forward, backward):" << "\n";
+		std::cin >> command;
+		if (movements.find(command) != movements.end()) {
+			movement = movements[command];
+			dxTotal += movement.first;
+			dyTotal += movement.second;
+			i++;
+		}
+		else {
+			std::cout << "Invalid command. Please try again." << "\n";
+		}
+	}
+	std::cout << "Tell me starting coordinates of x:" << "\n";
+	std::cin >> x;
+	std::cout << "Tell me starting coordinates of y:" << "\n";
+	std::cin >> y;
+	x = x + dxTotal;
+	y = y + dyTotal;
+	std::cout << "Final coordinates are: (" << x << ", " << y << ")" << "\n";
+	return 0;
 }
